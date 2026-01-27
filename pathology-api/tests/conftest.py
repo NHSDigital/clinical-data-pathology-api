d48fb25b64505b8ea4671f7b48bf6c733b94b5ae
Jack Wainwright <79214177+nhsd-jack-wainwright@users.noreply.github.com>
2026-01-27 17:58:22 +0000

    [CDAPI-55]: Added unused argument checking to Ruff config

--- pathology-api/tests/conftest.py
+++ pathology-api/tests/conftest.py
@@ -89,7 +89,7 @@ def hostname() -> str:
     return _fetch_env_variable("HOST", str)
 
 
-def _fetch_env_variable[T](name: str, t: type[T]) -> T:
+def _fetch_env_variable[T](name: str, _: type[T]) -> T:
     value = os.getenv(name)
     if not value:
         raise ValueError(f"{name} environment variable is not set.")
