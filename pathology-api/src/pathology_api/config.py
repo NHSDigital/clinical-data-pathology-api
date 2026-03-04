91501642d6f82171ed26002e5703d0e1e332ff1f
Jack Wainwright <79214177+nhsd-jack-wainwright@users.noreply.github.com>
2026-03-04 17:08:31 +0000

    [CDAPI-85]: Added initial plumbing for lambda to utilise provided environment variables

---
+++ pathology-api/src/pathology_api/config.py
@@ -0,0 +1,67 @@
+import os
+import re
+from dataclasses import dataclass
+from datetime import timedelta
+from enum import StrEnum
+
+
+class ConfigError(Exception):
+    pass
+
+
+class DurationUnit(StrEnum):
+    SECONDS = "s"
+    MINUTES = "m"
+
+
+@dataclass(frozen=True)
+class Duration:
+    def __init__(self, unit: DurationUnit, value: int):
+        self.unit = unit
+        self.value = value
+
+    @property
+    def timedelta(self) -> timedelta:
+        match self.unit:
+            case DurationUnit.SECONDS:
+                return timedelta(seconds=self.value)
+            case DurationUnit.MINUTES:
+                return timedelta(minutes=self.value)
+
+
+def get_optional_environment_variable[T](name: str, _type: type[T]) -> T | None:
+    value = os.getenv(name)
+
+    match _type:
+        case Duration() if value is not None:
+            parsed = re.fullmatch(r"(?P<value>\d+)(?P<unit>[sm])", value)
+            if parsed is None:
+                raise ConfigError(f"Invalid duration value: {value!r}")
+
+            raw_value = parsed.group("value")
+            raw_unit = parsed.group("unit")
+
+            if not raw_value or not raw_value.isdigit():
+                raise ConfigError(f"Invalid duration value: {value!r}")
+
+            return Duration(
+                unit=DurationUnit(raw_unit),
+                value=int(raw_value),
+            )
+
+        case _ if value is not None:
+            if not isinstance(value, _type):
+                raise ConfigError(
+                    f"Environment variable {name!r} is not of type {_type!r}"
+                )
+
+            return value
+        case _:
+            return None
+
+
+def get_environment_variable[T](name: str, _type: type[T]) -> T:
+    value = get_optional_environment_variable(name=name, _type=_type)
+    if value is None:
+        raise ConfigError(f"Environment variable {name!r} is not set")
+    return value
