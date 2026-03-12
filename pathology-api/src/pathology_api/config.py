54fe347997c131e6bb2a905b358b2a26c1467cd6
Jack Wainwright <79214177+nhsd-jack-wainwright@users.noreply.github.com>
2026-03-12 18:14:04 +0000

    [CDAPI-85]: Added unit tests for config.py
    Also refactored config.py to better support primitive types.

--- pathology-api/src/pathology_api/config.py
+++ pathology-api/src/pathology_api/config.py
@@ -1,9 +1,10 @@
 import os
 import re
+from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import timedelta
 from enum import StrEnum
-from typing import cast
+from typing import Any, cast
 
 
 class ConfigError(Exception):
@@ -29,34 +30,45 @@ class Duration:
                 return timedelta(minutes=self.value)
 
 
+_SUPPORTED_PRIMATIVES: dict[type[Any], Callable[[str], Any]] = {
+    str: str,
+    int: int,
+}
+
+
 def get_optional_environment_variable[T](name: str, _type: type[T]) -> T | None:
     value = os.getenv(name)
 
-    if _type is Duration and value is not None:
-        parsed = re.fullmatch(r"(?P<value>\d+)(?P<unit>[sm])", value)
-        if parsed is None:
-            raise ConfigError(f"Invalid duration value: {value!r}")
+    match _type:
+        case _ if _type is Duration:
+            if value is None:
+                return None
 
-        raw_value = parsed.group("value")
-        raw_unit = parsed.group("unit")
+            parsed = re.fullmatch(r"(?P<value>\d+)(?P<unit>[sm])", value)
+            if parsed is None:
+                raise ConfigError(f"Invalid duration value: {value!r}")
 
-        if not raw_value or not raw_value.isdigit():
-            raise ConfigError(f"Invalid duration value: {value!r}")
+            raw_value = parsed.group("value")
+            raw_unit = parsed.group("unit")
 
-        return cast(
-            "T",
-            Duration(
-                unit=DurationUnit(raw_unit),
-                value=int(raw_value),
-            ),
-        )
-    elif value is not None:
-        if not isinstance(value, _type):
-            raise ConfigError(f"Environment variable {name!r} is not of type {_type!r}")
+            return cast(
+                "T",
+                Duration(
+                    unit=DurationUnit(raw_unit),
+                    value=int(raw_value),
+                ),
+            )
 
-        return value
-    else:
-        return None
+        case _ if _type in _SUPPORTED_PRIMATIVES:
+            if value is None:
+                return None
+
+            return cast("T", _SUPPORTED_PRIMATIVES[_type](value))
+
+        case _:
+            raise ValueError(
+                f"Required type {_type} is not supported for config values"
+            )
 
 
 def get_environment_variable[T](name: str, _type: type[T]) -> T:
